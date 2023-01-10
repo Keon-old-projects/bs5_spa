bd1a1851ffa25e4e7a27bf9aff3318c5f044ec27
Keon <h21816595@gmail.com>
2023-01-10 16:23:43 +0800

    swiper 無限輪播

--- scripts/all.js
+++ scripts/all.js
@@ -17,14 +17,14 @@ $(function () {
                 }
             },
             {
-                breakpoint: 600,
+                breakpoint:768,
                 settings: {
                     slidesToShow: 2,
                     slidesToScroll: 2
                 }
             },
             {
-                breakpoint: 480,
+                breakpoint: 576,
                 settings: {
                     slidesToShow: 1,
                     slidesToScroll: 1
