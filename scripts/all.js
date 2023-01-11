20f9f8e33c4683a60da743bba6df0490c39080dc
Keon <h21816595@gmail.com>
2023-01-11 11:39:09 +0800

    index.html完成

--- scripts/all.js
+++ scripts/all.js
@@ -1,35 +1,56 @@
 $(function () {
-    $('.slick-slider').slick({
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 1000,
-        dots: true,
-        infinite: true,
-        responsive: [
-            {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 3,
-                    infinite: true,
-                    dots: true
-                }
-            },
-            {
-                breakpoint:768,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2
-                }
-            },
-            {
-                breakpoint: 576,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
+    // 新品上市輪播
+    $('.podcuct-swiper')
+        .empty()
+        .each(function(){
+            // console.log();
+            for (let i = 0; i < 10; i++) {
+                let num=i+12;
+                $(this).append(`
+                    <div class="card">
+                        <img src="https://picsum.photos/300/300/?random=${num}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <p class="card-text"><a href="#" class="text-decoration-none"><small
+                                        class="text-muted">類別</small></a></p>
+                            <h4 class="card-title">產品名稱</h4>
+                            <p class="card-text text-secondary">NT$487</p>
+                        </div>
+                    </div>
+                `)
+                
             }
-        ]
-    });
+        })
+        .slick({
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            autoplay: true,
+            autoplaySpeed: 1000,
+            dots: true,
+            infinite: true,
+            responsive: [
+                {
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 3,
+                        infinite: true,
+                        dots: true
+                    }
+                },
+                {
+                    breakpoint:768,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 2
+                    }
+                },
+                {
+                    breakpoint: 576,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
+                }
+            ]
+        });
 });
